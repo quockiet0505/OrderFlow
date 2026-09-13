ad799d76c219b4a94c7f73c2c1911ef90a583f22
quockiet0505 <dqkiet051104@gmail.com>
2026-09-14 00:27:08 +0700

    feat: initialize OrderFlow client application with PlaceOrder and Dashboard pages

--- src/Client/OrderFlow.Client/Program.cs
+++ src/Client/OrderFlow.Client/Program.cs
@@ -6,6 +6,11 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+var ordersUrl = builder.Configuration["ApiBaseUrls:OrdersApi"];
+var inventoryUrl = builder.Configuration["ApiBaseUrls:InventoryApi"];
 
+builder.Services.AddHttpClient("OrdersApi", client => client.BaseAddress = new Uri(ordersUrl ?? "http://localhost:5184"));
+builder.Services.AddHttpClient("InventoryApi", client => client.BaseAddress = new Uri(inventoryUrl ?? "http://localhost:5146"));
+
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("OrdersApi"));
 await builder.Build().RunAsync();
